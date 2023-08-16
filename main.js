903e538396016dc88ffe024a02e11cc4f668f2ca
ochoac47 <ochoac47@gmail.com>
2023-08-15 22:06:35 -0500

    Agregando correciones

--- main.js
+++ main.js
@@ -1,41 +1,27 @@
-let totalCompra = 0;
-let rta= ""
-rta = prompt("Ingrese ESC para salir. Aceptar para continuar.")
-const listaProductos = document.getElementById("lista-productos");
-const totalElement = document.getElementById("total");
-
-function agregarProducto() {
-  const productoSelect = document.getElementById("producto");
-  const productoValue = parseInt(productoSelect.value);
-
-  if (productoValue === 0) {
-    alert("Selecciona un producto válido");
-    return;
-  }
-
-  let descripcion = "";
-  let precio = 0;
-
-  switch (productoValue) {
-    case 15:
-      descripcion = "Dona Glaseada";
-      precio = 15;
-      break;
-    case 20:
-      descripcion = "Dona Fudge";
-      precio = 20;
-      break;
-    case 30:
-      descripcion = "Pack de Donas";
-      precio = 30;
-      break;
-  }
-
-  totalCompra += precio;
-  listaProductos.innerHTML += `<li>${descripcion} - $${precio}</li>`;
-  totalElement.textContent = totalCompra;
-
-  productoSelect.value = 0; // Reiniciar selección
+function mostrar_Producto_Precio(producto, precio, cantidad){
+  return "Producto: " + producto + "\tCantidad: x" + cantidad + "\tPrecio por unidad: $" + precio;
 }
 
+let detalle_compra = "";
+let totalCompra = 0;
+let rta = "";
 
+do {
+  let producto = prompt("Ingrese su producto");
+  if (producto === "ESC") {
+    break;
+  }
+
+  let cantidad = Number(prompt("Ingrese la cantidad de productos"));
+  let precio = Number(prompt("Ingrese el precio"));
+
+  detalle_compra = detalle_compra + mostrar_Producto_Precio(producto, precio, cantidad) + "\n";
+  totalCompra = totalCompra + precio * cantidad
+  alert (detalle_compra)
+  rta = prompt("Ingrese ESC para salir. ACEPTAR para continuar la compra.");
+} 
+
+
+while (rta !== "ESC");
+
+alert(detalle_compra + "\nTotal compra: $" + totalCompra);
